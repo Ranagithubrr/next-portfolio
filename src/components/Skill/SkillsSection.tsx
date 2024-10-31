430f3ae764e22e33e99a3df77aaad1ce4101aa63
ranagithubrr <ranarr.dev@gmail.com>
2024-10-31 15:29:40 +0600

    Fix: skill section fixed

--- src/components/Skill/SkillsSection.tsx
+++ src/components/Skill/SkillsSection.tsx
@@ -28,7 +28,7 @@ const floatingVariants = {
 
 const SkillsSection = () => {
   return (
-    <section id="skills" className="bg-gradient-to-b from-gray-800 to-gray-900 p-16">
+    <section id="skills" className="bg-gradient-to-b from-gray-800 to-gray-900 lg:p-16">
       <InViewMotion>
         <div
           className="max-w-7xl mx-auto text-center">          
@@ -50,7 +50,7 @@ const SkillsSection = () => {
                 className="bg-gray-800 p-4 rounded-lg shadow-lg hover:scale-105 transform transition duration-300"
                 variants={floatingVariants}
               >
-                <skill.Icon className={`${skill.color} lg:w-16 lg:h-16 mx-auto mb-2`} />
+                <skill.Icon className={`${skill.color} w-12 h-12 lg:w-16 lg:h-16 mx-auto mb-2`} />
                 <h3 className="lg:text-lg font-semibold text-teal-400">{skill.name}</h3>
               </motion.div>
             ))}
